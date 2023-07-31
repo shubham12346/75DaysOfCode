c8af1b0218d12d90e3ef9082a8bf0fa8e1fe099e
Shubham Agrahari <aakashagrahari26@gmail.com>
2023-07-31 19:49:09 +0530

    day3

---
+++ day3.js
@@ -0,0 +1,38 @@
+// 1431. Kids With the Greatest Number of Candies
+// Easy
+// 3.6K
+// 436
+// Companies
+// There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+// Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+// Note that multiple kids can have the greatest number of candies.
+// Example 1:
+// Input: candies = [2,3,5,1,3], extraCandies = 3
+// Output: [true,true,true,false,true] 
+// Explanation: If you give all extraCandies to:
+// - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+// - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+// - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+// - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+// - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+function kidsWithCandies(candies, extraCandies) {
+    var kidWithGreatestCandy = [];
+    var maxElement = Number.MIN_VALUE;
+    for (var index = 0; index < candies.length; index++) {
+        maxElement = Math.max(maxElement, candies[index]);
+    }
+    for (var _i = 0, candies_1 = candies; _i < candies_1.length; _i++) {
+        var candy = candies_1[_i];
+        var candyAfterAddingExtraCandy = candy + extraCandies;
+        if (candyAfterAddingExtraCandy < maxElement) {
+            kidWithGreatestCandy.push(false);
+        }
+        else {
+            kidWithGreatestCandy.push(true);
+        }
+    }
+    return kidWithGreatestCandy;
+}
+;
+var getCandyResult = kidsWithCandies([2, 3, 5, 1, 3], 3);
+console.log(getCandyResult);
