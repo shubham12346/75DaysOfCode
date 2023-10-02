4663ef2173f99d2171f9d7206dcc15206b2da762
Shubham Agrahari <aakashagrahari26@gmail.com>
2023-10-02 16:18:23 +0530

    day 66

--- day66.ts
+++ day66.ts
@@ -10,13 +10,12 @@
 // Insert a character
 // Delete a character
 // Replace a character
- 
 
 // Example 1:
 
 // Input: word1 = "horse", word2 = "ros"
 // Output: 3
-// Explanation: 
+// Explanation:
 // horse -> rorse (replace 'h' with 'r')
 // rorse -> rose (remove 'r')
 // rose -> ros (remove 'e')
@@ -24,44 +23,46 @@
 
 // Input: word1 = "intention", word2 = "execution"
 // Output: 5
-// Explanation: 
+// Explanation:
 // intention -> inention (remove 't')
 // inention -> enention (replace 'i' with 'e')
 // enention -> exention (replace 'n' with 'x')
 // exention -> exection (replace 'n' with 'c')
 // exection -> execution (insert 'u')
- 
 
 // Constraints:
 
 // 0 <= word1.length, word2.length <= 500
 // word1 and word2 consist of lowercase English letters.
 
-
 function minDistance(word1: string, word2: string): number {
-    if ((word1 && !word2) || (!word1 && word2)) {
-     return word1 ? word1.length : word2.length;
- }
- 
- let word1Length = word1.length;
- let word2Length = word2.length;
- const dp = new Array(word1Length + 1).fill(0).map(() => new Array(word2Length + 1).fill(0));
+  if ((word1 && !word2) || (!word1 && word2)) {
+    return word1 ? word1.length : word2.length;
+  }
 
- for (let word1Index = 0; word1Index <= word1Length; word1Index++) {
+  let word1Length = word1.length;
+  let word2Length = word2.length;
+  const dp = new Array(word1Length + 1)
+    .fill(0)
+    .map(() => new Array(word2Length + 1).fill(0));
+
+  for (let word1Index = 0; word1Index <= word1Length; word1Index++) {
     dp[word1Index][0] = word1Index;
- }
- for (let word2Index = 0; word2Index <= word2Length; word2Index++) {
+  }
+  for (let word2Index = 0; word2Index <= word2Length; word2Index++) {
     dp[0][word2Index] = word2Index;
- }
+  }
 
- for (let row = 1; row <= word1Length; row++) {
+  for (let row = 1; row <= word1Length; row++) {
     for (let col = 1; col <= word2Length; col++) {
-       if (word1[row - 1] === word2[col - 1]) {
-          dp[row][col] = dp[row - 1][col - 1];
-       } else {
-          dp[row][col] = 1 + Math.min(dp[row - 1][col], dp[row][col - 1], dp[row - 1][col - 1]);
-       }
+      if (word1[row - 1] === word2[col - 1]) {
+        dp[row][col] = dp[row - 1][col - 1];
+      } else {
+        dp[row][col] =
+          1 +
+          Math.min(dp[row - 1][col], dp[row][col - 1], dp[row - 1][col - 1]);
+      }
     }
- }
- return dp[word1Length][word2Length];
-};
+  }
+  return dp[word1Length][word2Length];
+}
